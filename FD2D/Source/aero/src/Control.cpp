5e919e413eb36fcb42765265da096816e47acc3e
Clawedialt <ph.papaloucas@gmail.com>
2024-10-15 19:17:22 +0300

    z

--- FD2D/Source/aero/src/Control.cpp
+++ FD2D/Source/aero/src/Control.cpp
@@ -1,103 +1,115 @@
 #include "Control.h"
 
-Control::Control(const UAV& uav, const EngineMap& engine, double max_amps)
-:_engine(engine),_uav(uav), _aero(Aerodynamics()), _max_amps(max_amps), _current_thrust_mode(MAX){};
+Control::Control(const UAV &uav, const EngineMap &engine, double max_amps)
+    : _engine(engine), _uav(uav), _aero(Aerodynamics()), _max_amps(max_amps),
+      _current_thrust_mode(MAX){};
 
-    //set control for stuff that need polynomyal
-void Control::setControlMode(ControlMode control_mode, std::array<double, 4> coeffs){
-    if (control_mode == THETA){
-        _current_control_mode = control_mode;
-        _poly = Poly<double, _n_coeffs>(coeffs);
-    }
-    else{
-        throw std::invalid_argument("Invalid control mode set. Use the appropriate setControlMode method which does NOT ask for a polynomial");
-    }
+// set control for stuff that need polynomyal
+void Control::setControlMode(ControlMode control_mode,
+                             std::array<double, 4> coeffs) {
+  if (control_mode == THETA) {
+    _current_control_mode = control_mode;
+    _poly = Poly<double, _n_coeffs>(coeffs);
+  } else {
+    throw std::invalid_argument(
+        "Invalid control mode set. Use the appropriate setControlMode method "
+        "which does NOT ask for a polynomial");
+  }
 };
-//set control for stuff that dont need polynomial
-void Control::setControlMode(ControlMode control_mode){
-    if (control_mode != THETA)
-        _current_control_mode = control_mode;
-    else{
-        throw std::invalid_argument("Invalid control mode set. Use the appropriate setControlMode method which asks for a polynomial");
-    }
+// set control for stuff that dont need polynomial
+void Control::setControlMode(ControlMode control_mode) {
+  if (control_mode != THETA)
+    _current_control_mode = control_mode;
+  else {
+    throw std::invalid_argument(
+        "Invalid control mode set. Use the appropriate setControlMode method "
+        "which asks for a polynomial");
+  }
 }
 
-
-double Control::_getTheta(std::array<double, 2> velocity, double t, bool apply_ground_effect, double height){
-    double theta = 0;
-    #ifdef DEBUG
-        if(VERBOSITY_LEVEL>=3){
-            std::cout << "CONTROL MODE = " << _current_control_mode << std::endl;
-        }
-    #endif
-    switch (_current_control_mode){
-        case TAKEOFF:
-            theta =  _uav.getAoaTakeoff();
-            break;
-        case TRIM_GAMMA:
-            theta = _aero.getThetaForTrim(_uav, 
-                                            velocity,
-                                            *(this), apply_ground_effect, height);
-            if (abs(theta) > _max_theta){
-                //throw std::runtime_error("Theta exceeds the maximum allowed value");
-                theta = _max_theta;
-            }
-
-            break;
-        case THETA:
-            theta = _poly.getValue(t);
-            if (abs(theta) > _max_theta){
-                //throw std::runtime_error("Theta exceeds the maximum allowed value");
-                theta = _max_theta;
-            }
-            break;
-
-    }
-    return theta;
-}
-
-void Control::_applyControl(ControlState &control_state, std::array<double,2 > velocity, double t, bool apply_ground_effect, double height){
-    control_state.theta = _getTheta(velocity, t, apply_ground_effect, height);
-    control_state.thrust = getThrust(Aerodynamics::rotateFromEarth2Bodyframe(velocity, control_state.theta));
-
-
-}
-
-std::array<double, 2> Control::getForces(const State &state, ControlState& control_state, double t){
-    std::array<double, 2> velocity = {state.u, state.w};
-    _applyControl(control_state, velocity, t, apply_ground_effect, state.z + _uav.getWheelOffset());
-
-    std::array<double, 2> u = _aero.getAeroForcesEarthframe(_uav, velocity, control_state.theta, apply_ground_effect, state.z + _uav.getWheelOffset());
-    std::array<double, 2> thrust_earthaxis = Aerodynamics::rotateFromBody2Earthframe({control_state.thrust, 0}, -control_state.theta);
-    u[0] += thrust_earthaxis[0];
-    u[1] += thrust_earthaxis[1];
-
-    return u;
-}
-
-    void Control::applyBoundaries(std::array<double, 2> &forces, double altitude){
-        
-        if ( (forces[1] < 1.3*_uav.getTotalMass()*9.81) && (altitude < 0.01) && (_current_control_mode == TAKEOFF)){
-            forces[1] = _uav.getTotalMass()*9.81;
-        }
+double Control::_getTheta(std::array<double, 2> velocity, double t,
+                          bool apply_ground_effect, double height) {
+  double theta = 0;
+#ifdef DEBUG
+  if (VERBOSITY_LEVEL >= 3) {
+    std::cout << "CONTROL MODE = " << _current_control_mode << std::endl;
+  }
+#endif
+  switch (_current_control_mode) {
+  case TAKEOFF:
+    theta = _uav.getAoaTakeoff();
+    break;
+  case TRIM_GAMMA:
+    theta = _aero.getThetaForTrim(_uav, velocity, *(this), apply_ground_effect,
+                                  height);
+    if (abs(theta) > _max_theta) {
+      // throw std::runtime_error("Theta exceeds the maximum allowed value");
+      theta = _max_theta;
     }
 
-    bool Control::checkTermination(const State &state, std::array<double, 2> u){
-        switch (_current_control_mode){
-            case TAKEOFF:
-            if (u[1] > 1.3*_uav.getTotalMass()*9.81)
-                return true;
-            break;
-        }
-
-        switch(_termination_mode){
-            case REACH_CEIL:
-            if (state.z >= _ceil)
-                return true;
-            break;
-        }
-        return false;
+    break;
+  case THETA:
+    theta = _poly.getValue(t);
+    if (abs(theta) > _max_theta) {
+      // throw std::runtime_error("Theta exceeds the maximum allowed value");
+      theta = _max_theta;
     }
+    break;
+  }
+  return theta;
+}
 
+void Control::_applyControl(ControlState &control_state,
+                            std::array<double, 2> velocity, double t,
+                            bool apply_ground_effect, double height) {
+  control_state.theta = _getTheta(velocity, t, apply_ground_effect, height);
+  control_state.thrust = getThrust(
+      Aerodynamics::rotateFromEarth2Bodyframe(velocity, control_state.theta));
+}
 
+std::array<double, 2>
+Control::getForces(const State &state, ControlState &control_state, double t) {
+  std::array<double, 2> velocity = {state.u, state.w};
+  _applyControl(control_state, velocity, t, apply_ground_effect,
+                state.z + _uav.getWheelOffset());
 
+  std::array<double, 2> u = _aero.getAeroForcesEarthframe(
+      _uav, velocity, control_state.theta, apply_ground_effect,
+      state.z + _uav.getWheelOffset());
+  std::array<double, 2> thrust_earthaxis =
+      Aerodynamics::rotateFromBody2Earthframe({control_state.thrust, 0},
+                                              -control_state.theta);
+  u[0] += thrust_earthaxis[0];
+  u[1] += thrust_earthaxis[1];
+
+  return u;
+}
+
+void Control::applyBoundaries(std::array<double, 2> &forces, double altitude) {
+
+  if ((forces[1] < 1.3 * _uav.getTotalMass() * 9.81) && (altitude < 0.01) &&
+      (_current_control_mode == TAKEOFF)) {
+    forces[1] = _uav.getTotalMass() * 9.81;
+  }
+}
+
+bool Control::checkTermination(const State &state, std::array<double, 2> u) {
+  switch (_current_control_mode) {
+  case TAKEOFF:
+    if (u[1] > 1.3 * _uav.getTotalMass() * 9.81)
+      return true;
+    break;
+  }
+
+  switch (_termination_mode) {
+  case REACH_CEIL:
+    if (state.z >= _termination_value)
+      return true;
+    break;
+  case REACH_DISTANCE:
+    if (state.x >= _termination_value)
+      return true;
+    break;
+  }
+  return false;
+}
--- FD2D/tests/1-takeoff.cpp
+++ FD2D/tests/1-takeoff.cpp
@@ -1,40 +1,39 @@
-#include "UAV.h"
-#include "EngineMap.h"
-#include "StateSpace.h"
-#include "State.h"
 #include "Control.h"
+#include "EngineMap.h"
+#include "State.h"
+#include "StateSpace.h"
+#include "UAV.h"
 #include <iostream>
 
-int main(){
-    std::cout <<"Test executable for takeoff (classes UAV, Aerodynamics, State, StateSpace\n";
 
-    double S = 0.5;
-    double AR = 9;
-    double a0 = Aerodynamics::deg2rad(-4);
-    double mass = 10;
-    UAV p1(S, AR, mass, a0, 1);
-    //p1.setAoaTakeoff(Aerodynamics::deg2rad(0));
-    Aerodynamics aero(p1);
+int main() {
+  std::cout << "Test executable for takeoff (classes UAV, Aerodynamics, State, "
+               "StateSpace\n";
 
+  double S = 0.5;
+  double AR = 9;
+  double a0 = Aerodynamics::deg2rad(-4);
+  double mass = 10;
+  UAV p1(S, AR, mass, a0, 1);
+  // p1.setAoaTakeoff(Aerodynamics::deg2rad(0));
+  Aerodynamics aero(p1);
 
-    StateHistory history;
-    StateSpace sim(history);
-    sim.setParameters(0, 0.1, 100);
-    State init_state;
-    ControlState control_state;
-    EngineMap engine(EngineMap::P12x6);
+  StateHistory history;
+  StateSpace sim(history);
+  sim.setParameters(0, 0.1, 100);
+  State init_state;
+  ControlState control_state;
+  EngineMap engine(EngineMap::P12x6);
 
+  Control control(engine, p1, 30);
+  control.setControlMode(Control::TAKEOFF);
 
-    Control control(engine, p1, 30);
-    control.setControlMode(Control::TAKEOFF);
+  std::cout << "Solving takeof for initial state: " << init_state << std::endl;
+  sim.solve(p1, init_state, control, control_state);
+  // sim.solve2(p1, init_state, control, control_state);
 
-    std::cout << "Solving takeof for initial state: " << init_state << std::endl;
-    sim.solve(p1, init_state, control, control_state);
-    //sim.solve2(p1, init_state, control, control_state);
+  history.printToCsv("takeoff_test");
+  // history2.printToCsv("RK4new");
 
-
-    history.printToCsv("takeoff_test");
-    //history2.printToCsv("RK4new");
-
-    return 0;
+  return 0;
 }
