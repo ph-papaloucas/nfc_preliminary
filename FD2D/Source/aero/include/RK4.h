82e0a410c888cb6cbe99b7391f3e3936be5eef85
Clawedialt <ph.papaloucas@gmail.com>
2024-10-16 13:52:23 +0300

    IMPORTANT! Corrected a bug in RK4!!!!!!

--- FD2D/Source/aero/include/RK4.h
+++ FD2D/Source/aero/include/RK4.h
@@ -39,53 +39,54 @@ using DynFunc = std::array<double, neq> (*)(
 
 
 
-template <std::size_t neq, std::size_t nin, std::size_t nargs> std::array<double, neq> 
-stepRK4(DynFunc<neq, nin, nargs> dyn_func,
-const double t, const double dt, 
-const std::array<double, neq>& x_prev, 
-const std::array<double, nin>& u,
-const std::array<double, nargs>& args){
+template <std::size_t neq, std::size_t nin, std::size_t nargs>
+std::array<double, neq> stepRK4(DynFunc<neq, nin, nargs> dyn_func,
+                                     const double t, const double dt,
+                                     const std::array<double, neq> &x_prev,
+                                     const std::array<double, nin> &u,
+                                     const std::array<double, nargs> &args) {
+  std::cout << "startstep\n";
+  // RK4 coeffs
+  static const std::array<double, 4> RK4 = {1.0 / 6.0, 2.0 / 6.0, 2.0 / 6.0,
+                                            1.0 / 6.0};
 
+  std::array<std::array<double, neq>, neq> K = {};
 
-    //RK4 coeffs
-    static const std::array<double, 4> RK4 = {
-        1.0 / 6.0,
-        2.0 / 6.0,
-        2.0 / 6.0,
-        1.0 / 6.0
-    };
+  // placeholders
+  double t_2 = t + 0.5 * dt;
+  double t_3 = t + dt;
 
-    std::array<std::array<double, neq>, neq> K = {};
+  // at each timestep, forward iterations
+  std::array<double, neq> x_temp = x_prev;
+  std::array<double, neq> xdot = {};
 
-    //placeholders
-    double t_2 = t + 0.5 * dt;
-    double t_3 = t + dt;
+  int step = 0; // 0 -> 1
+  xdot = dyn_func(x_temp, u, args);
+  for (int ieq = 0; ieq < neq; ieq++) {
+    K[step][ieq] = dt * xdot[ieq];
+    x_temp[ieq] = x_prev[ieq] + 0.5 * K[step][ieq];
+  }
 
-    //at each timestep, forward iterations
-    std::array<double, neq> x_temp = x_prev;
+  step = 1; // 1 -> 2
+  xdot = dyn_func(x_temp, u, args);
+  for (int ieq = 0; ieq < neq; ieq++) {
+    K[step][ieq] = dt * xdot[ieq];
+    x_temp[ieq] = x_prev[ieq] + 0.5 * K[step][ieq];
+  }
+  step = 2; // 2 -> 3
+  xdot = dyn_func(x_temp, u, args);
+  for (int ieq = 0; ieq < neq; ieq++) {
+    K[step][ieq] = dt * xdot[ieq];
+    x_temp[ieq] = x_prev[ieq] + K[step][ieq];
+  }
 
-    int step = 0; //0 -> 1
-    for (int ieq = 0; ieq < neq; ieq++) {
-        K[step][ieq] = dt * dyn_func(x_temp, u, args)[ieq];
-        x_temp[ieq] = x_prev[ieq] + 0.5 * K[step][ieq];
-    }
+  step = 3; // 3->4
+  xdot = dyn_func(x_temp, u, args);
+  for (int ieq = 0; ieq < neq; ieq++) {
+    K[step][ieq] = dt * xdot[ieq];
+    x_temp[ieq] = x_prev[ieq] + RK4[0] * K[0][ieq] + RK4[1] * K[1][ieq] +
+                  RK4[2] * K[2][ieq] + RK4[3] * K[3][ieq]; // update state
+  }
 
-    step = 1;   //1 -> 2
-    for (int ieq = 0; ieq < neq; ieq++) {
-        K[step][ieq] = dt * dyn_func(x_temp, u, args)[ieq];
-        x_temp[ieq] = x_prev[ieq] + 0.5 * K[step][ieq];
-    }
-    step = 2;   //2 -> 3
-    for (int ieq = 0; ieq < neq; ieq++) {
-        K[step][ieq] = dt * dyn_func(x_temp, u, args)[ieq];
-        x_temp[ieq] = x_prev[ieq] + K[step][ieq];
-    }
-
-    step = 3; //3->4
-    for (int ieq = 0; ieq < neq; ieq++) {
-        K[step][ieq] = dt * dyn_func(x_temp, u,args)[ieq];
-        x_temp[ieq] = x_prev[ieq] + RK4[0] * K[0][ieq] + RK4[1]  * K[1][ieq] + RK4[2]  * K[2][ieq] + RK4[3]  * K[3][ieq]; //update state
-    }
-
-    return x_temp;
+  return x_temp;
 }
